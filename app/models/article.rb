50ac1e88a7d8e68469c4750bb9fc95f6925f3dfd
Muhammad Shayan <mshayanshafi@gmail.com>
2016-02-19 04:42:59 +0000

    Initial files

---
+++ app/models/article.rb
@@ -0,0 +1,14 @@
+class Article < ActiveRecord::Base
+
+     validates :title, presence: true,
+                    length: { minimum: 5 }
+     
+     
+     validates :title, presence: true,
+                    length: { minimum: 5 }
+                    
+     
+     validates :title, presence: true,
+                    length: { minimum: 5 }
+
+end
